79cb3fd05c80a6f929405612014e7fb584757302
Monirul Hasan <kmhasan@gmail.com>
2018-08-11 09:29:58 +0600

    vaadin binding validation

--- vaadin-demo/src/main/java/bd/ac/seu/aj/vaadindemo/ui/HelloUI.java
+++ vaadin-demo/src/main/java/bd/ac/seu/aj/vaadindemo/ui/HelloUI.java
@@ -3,6 +3,7 @@ package bd.ac.seu.aj.vaadindemo.ui;
 import bd.ac.seu.aj.vaadindemo.model.Student;
 import bd.ac.seu.aj.vaadindemo.service.StudentService;
 import com.vaadin.annotations.Theme;
+import com.vaadin.data.Binder;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
@@ -21,11 +22,19 @@ public class HelloUI extends UI {
         this.studentService = studentService;
     }
 
+    private boolean isNumber(String string) {
+        for (int i = 0; i < string.length(); i++)
+            if (!Character.isDigit(string.charAt(i)))
+                return false;
+        return true;
+    }
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         Label label = new Label("Hello World");
         Button button = new Button("Click Me");
         Grid<Student> studentGrid = new Grid<>();
+        studentGrid.setCaption("List of Students");
         studentGrid.setItems(studentService.getAllStudents());
         studentGrid.addColumn(Student::getId).setCaption("Student ID");
         studentGrid.addColumn(Student::getName).setCaption("Student Name");
@@ -44,12 +53,53 @@ public class HelloUI extends UI {
             selectedSetudentSet.stream().forEach(System.out::println);
         });
 
+        FormLayout studentForm = new FormLayout();
+        studentForm.setCaption("Student Form");
+        TextField idField = new TextField("Student ID");
+        TextField nameField = new TextField("Student Name");
+        TextField cgpaField = new TextField("CGPA");
+        Button saveButton = new Button("Save");
+        studentForm.addComponents(idField, nameField, cgpaField, saveButton);
+
+        Binder<Student> studentBinder = new Binder<>();
+        //studentBinder.bind(nameField, Student::getName, Student::setName);
+        studentBinder
+                .forField(nameField)
+                .asRequired("Names cannot be empty")
+                .withValidator(s -> s.length() >= 3, "Names should be at least 3 letters long")
+                .bind(Student::getName, Student::setName);
+
+        studentBinder
+                .forField(idField)
+                .asRequired()
+                .withValidator(s -> s.length() == 4, "Student ID should be 4 digit long")
+                .withValidator(s -> isNumber(s), "Student ID must be a number")
+                .bind(student -> student.getId() + "",
+                        (student, s) -> student.setId(Long.parseLong(s)));
+
+        // add a binder for the CGPA
+        // has to be a number in the range [0.00, 4.00]
+        
+        saveButton.addClickListener(clickEvent -> {
+            Student student = new Student();
+            studentBinder.writeBeanIfValid(student);
+            Notification.show(student.toString());
+        });
+/*
+        // JavaFX Example
+        // on click of some button
+        long id = Long.parseLong(idField.getText());
+        String name = nameField.getText();
+        double cgpa = Double.parseDouble(cgpaField.getText());
+        Student student = new Student(id, name, cgpa);
+*/
+
         VerticalLayout verticalLayout = new VerticalLayout();
         HorizontalLayout horizontalLayout = new HorizontalLayout();
         horizontalLayout.addComponents(label, button, waiverButton);
 
         verticalLayout.addComponent(horizontalLayout);
-        verticalLayout.addComponent(studentGrid);
+        verticalLayout.addComponents(studentForm, studentGrid);
 
         setContent(verticalLayout);
     }
