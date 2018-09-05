7d57ef3eedb8f737efe4f2fd87fce2b13e692ac4
Monirul Hasan <kmhasan@gmail.com>
2018-09-05 12:58:55 +0600

    security

---
+++ spring-boot-demo/src/main/java/bd/ac/seu/aj/springbootdemo/model/User.java
@@ -0,0 +1,51 @@
+package bd.ac.seu.aj.springbootdemo.model;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class User implements UserDetails {
+    private String username;
+    private String password;
+    private String role;
+
+    private boolean enabled;
+    private boolean locked;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
